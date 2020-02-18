ef3f9897a313df63a5b10085277db74c2ce8f1ec
NovaYin <laciepark300323@hotmail.com>
2020-02-18 17:12:59 +0100

    Added Player Movement
    Added player movement and made changes to the gravity pull

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,31 +4,83 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public float planetMass;
-    public float playerMass;
+    /// <summary>
+    /// Since the player isn't going to be the only one affected bby the planet's gravity
+    /// A list is needed to store all the rigidbodies affected.
+    /// </summary>
+    private List<Rigidbody2D> rgList = new List<Rigidbody2D>();
 
-    Rigidbody2D rgPlayer;
+    private float totRadius; // Calculates the radius of the collider
+    [SerializeField] private CircleCollider2D radiusComp;
 
-    public float distance;
-    public Transform centerOfPlanet;
-    public float forceValue;
-    public float gForce;
-    Vector3 forceDirection;
+    /// <summary>
+    /// Gravitational pull of the planet
+    /// </summary>
+    [SerializeField] private float maxGForce = 9.81f; // force value maximal
+    [SerializeField] private float minGForce = 1; // force value minimum
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        rgPlayer = GetComponent<Rigidbody2D>();
-        playerMass = rgPlayer.mass;
-        distance = Vector3.Distance(centerOfPlanet.transform.position, transform.position);
-        forceValue = gForce * (planetMass * playerMass) / (distance * distance);
+        
+        totRadius = radiusComp.radius * radiusComp.gameObject.transform.lossyScale.x; // Because im using a collider within the parent object, the scale is different on the chil object
+    }   
 
+    private void FixedUpdate()
+    {
+        /// <summary>
+        /// Since the player isn't going to be the only one affected bby the planet's gravity
+        /// A list is needed to store all the rigidbodies affected.
+        /// </summary>
+
+        foreach (Rigidbody2D rgbody in rgList)
+        {
+            /// <summary>
+            /// Calculate how much gravity to add with the force.
+            /// </summary>
+            Vector2 forceDirection = ((Vector2)transform.position - rgbody.position).normalized; //Calculates in which direction force needs to be applied
+
+            float distance = 1 - Vector2.Distance(transform.position, rgbody.position) / totRadius; //Calculate in precentages distance in the collider towards the center of the planet
+           // Debug.LogFormat("{0} dist {1} radius", Vector2.Distance(transform.position, rgbody.position), totRadius);
+
+            float gForceLerp = Mathf.Lerp(minGForce, maxGForce, distance);
+
+            Vector2 gravityForce = forceDirection * gForceLerp;
+            //Debug.Log(gravityForce);
+
+            // Add the force of gravity to the rigidbody of the object.
+            //ToDo: Add minus to a button / keyinput to change attraction to repulsion
+            rgbody.AddForce(gravityForce);
+
+            // Rotate whatever is affected to face away from the center at all times.
+            rgbody.transform.up = forceDirection * -1;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// A planet has a atmosphere, in which the area is affected. The closer you get to the center the harder the gravity pulls.
+    /// To simulate this Atmospheric effect, a OntriggerEnter and OntriggerExit will be created.
+    /// </summary>
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        forceDirection = (centerOfPlanet.position - transform.position).normalized; 
-        rgPlayer.AddForce(forceValue * forceDirection);
+        Rigidbody2D affectedByGravity = collision.GetComponent<Rigidbody2D>(); //Get the rigidbody of the object entering the Collider of the planet.
+
+        //If there's a rigidbody detected
+        if (affectedByGravity)
+        {
+            rgList.Add(affectedByGravity); //Add the rigidbody to the list of that particluar planet
+            Debug.Log(affectedByGravity.gameObject.name + " entered the gravity of " + this.gameObject.name); //Log for check
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Rigidbody2D previousByGravity = collision.GetComponent<Rigidbody2D>(); //Get the rigidbody of the object exiting the Collider of the planet.
+
+        if (previousByGravity && rgList.Contains(previousByGravity))
+        {
+            rgList.Remove(previousByGravity); //Remove the rigidbody from the list of that particular plannet
+            Debug.Log(previousByGravity.gameObject.name + " exited the gravity of " + this.gameObject.name); //Log for check
+        }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidb; //Rigidbody of the player
+    [SerializeField] private float speed = 1f; //Movement speed of the character
+    [SerializeField] private float jumpheight = 3f; //Jumpheight of the character
+
+    private bool isGrounded = false; // Checks if the character is touching ground or not. Default is false.
+    private Vector2 moveInput = Vector2.zero; // Writes the Vector 2 (0,0)
+
+    private void Start()
+    {
+        //rigidb.velocity = new Vector2(2, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        /// <summary>
+        /// Movement based on the horiontal axis.
+        /// </summary>
+        Vector2 movementHor = this.transform.right * moveInput.x * speed;
+        rigidb.AddForce(movementHor);
+
+        /// <summary>
+        /// Movement based on the horiontal axis.
+        /// </summary>
+        Vector2 movementVert = this.transform.up * moveInput.y * jumpheight;
+        rigidb.AddForce(movementVert, ForceMode2D.Impulse);
+        isGrounded = false; //since the player is jumping, it's no longer grounded.
+
+        moveInput = Vector2.zero; ;
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal"); // Uses the horizontal input as for th X axis
+
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space)) //If the player is grounded and theres in input key of SPACE 
+        {
+            moveInput.y = 1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+        Debug.Log(this.gameObject.name + " is grounded ");
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true; // As long as the character is on top of the collision box, the character will remain grounded
+    }
+}
