7e39bbfb0b42af4b1776b40d7a5af1348b0b4c8b
NovaYin <laciepark300323@hotmail.com>
2020-02-24 20:34:40 +0100

    Added Portal Code

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private Rigidbody2D enterRigidbody;
+    private float enterVelocity, exitVelocity;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        enterRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+        enterVelocity = enterRigidbody.velocity.x;
+
+        if (gameObject.name == "startPortal")
+        {
+            PortalControl.portalControlInstance.disableCollider("start");
+            PortalControl.portalControlInstance.createClone("atStart");
+        }
+        else if (gameObject.name == "exitPortal")
+        {
+            PortalControl.portalControlInstance.disableCollider("exit");
+            PortalControl.portalControlInstance.createClone("atExit");
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        exitVelocity = enterRigidbody.velocity.x;
+
+        if (enterVelocity != exitVelocity)
+        {
+            Destroy(GameObject.Find("PlayerClone"));
+        }
+        else if (gameObject.name != "PlayerClone")
+        {
+            Destroy(collision.gameObject);
+            PortalControl.portalControlInstance.enableCollider();
+            GameObject.Find("PlayerClone").name = "Player";
+        }
+    }
+}
---
+++ Assets/Scripts/PortalControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalControl : MonoBehaviour
+{
+    public static PortalControl portalControlInstance; //singleton
+
+    [SerializeField] private GameObject portalEnter, portalExit;
+    [SerializeField] private Transform portalEnterSpawn, portalExitSpawn;
+    private Collider2D portalEnterCollider, portalExitCollider;
+
+    [SerializeField] private GameObject clonePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        portalControlInstance = this;
+        portalEnterCollider = portalEnter.GetComponent <Collider2D>();
+        portalExitCollider = portalExit.GetComponent<Collider2D>();
+    }
+
+    public void createClone (string spawnPoint)
+    {
+        if (spawnPoint == "atExit")
+        {
+            var instantiatePlayer = Instantiate(clonePlayer, portalExitSpawn.position, Quaternion.identity);
+            instantiatePlayer.gameObject.name = "PlayerClone";
+        }
+
+        if(spawnPoint == "atStart")
+        {
+            var instantiatePlayer = Instantiate(clonePlayer, portalEnterSpawn.position, Quaternion.identity);
+            instantiatePlayer.gameObject.name = "PlayerClone";
+        }
+    }
+
+    public void disableCollider(string colliderToDisable)
+    {
+        if (colliderToDisable == "start")
+        {
+            portalEnterCollider.enabled = false;
+        }
+        if (colliderToDisable == "exit")
+        {
+            portalExitCollider.enabled = false;
+        }
+    }
+    
+    public void enableCollider()
+    {
+        portalEnterCollider.enabled = true;
+        portalExitCollider.enabled = true;
+    }
+}
