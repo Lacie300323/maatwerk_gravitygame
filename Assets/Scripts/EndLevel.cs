de200b690a4bdade3a8f11e325640629f7c670a8
Nova Yin <laciepark300323@hotmail.com>
2020-03-08 18:06:23 +0100

    Added Winning Level component
    Added UI, UI animations and triggers for completing a level.

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    { 
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hi");
+            gameManager.CompleteLevel();
+        }
+    }
+}
