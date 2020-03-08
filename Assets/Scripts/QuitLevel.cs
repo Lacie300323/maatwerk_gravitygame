bc11f6b4f0e895eefc5c66e44fedfd65660d8509
Nova Yin <laciepark300323@hotmail.com>
2020-03-08 20:56:08 +0100

    Main Menu and setup
    Added main menu assets and mock-up. Added button functionalities

---
+++ Assets/Scripts/QuitLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitLevel : MonoBehaviour
+{
+    public void quitGame()
+    {
+        Debug.Log("Quited the game");
+        Application.Quit();
+    }
+}
