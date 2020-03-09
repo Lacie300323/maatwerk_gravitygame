b36c8b797fd27a6a8059dfdbc9d4d88339cd2310
Nova Yin <laciepark300323@hotmail.com>
2020-03-09 23:27:12 +0100

    Added static enemy and start to set boundary
    Added a static enemy which shoots in intervals of 5s and added a start to boundaries and respawning when outside of boundaries

---
+++ Assets/Scripts/Boundaries.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name + " has entered the boundaries");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {            
+            Debug.Log(collision.name + " has exited the boundaries");
+            collision.gameObject.GetComponent<Health>().TakeDamage(1);
+            CheckpointManager.instance.RestorePlayerToCheckpoint();
+
+            Player playerScript = collision.gameObject.GetComponent<Player>();
+            playerScript.inverseGravity = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float timeBtwShots;
+
+    public float startTimebtwShots;
+    public float withinDistance;
+
+    public GameObject projectile;
+    public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimebtwShots;
+    }
+
+
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, player.position) <= withinDistance)
+        {
+            if (timeBtwShots <= 0)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBtwShots = startTimebtwShots;
+            }
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
+        }
+    
+    }
+}
