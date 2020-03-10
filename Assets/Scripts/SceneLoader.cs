23f20608151583b49d3c74fe828fad1d4c5f2229
Nova Yin <laciepark300323@hotmail.com>
2020-03-10 20:51:37 +0100

    Added Level Select
    added level select menu to the first level of the main menu, mockup

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader instance;
+    private void Awake()
+    {
+        // If a Sceneloader Manager already exists (and it is not this object), delete this
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        // If it doesn't exist yet, make this the only Sceneloader Manager to exist
+        else if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
